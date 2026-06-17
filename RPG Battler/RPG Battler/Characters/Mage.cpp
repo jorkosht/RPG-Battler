d425925b1ee7dcb08cee0191d45cd824829d6c9e
Jorkosht <118050451+jorkosht@users.noreply.github.com>
2026-06-17 16:56:56 +0300

    Delete RPG Battler/RPG Battler/Characters/Mage.cpp

--- RPG Battler/RPG Battler/Characters/Mage.cpp
+++
@@ -1,9 +0,0 @@
-#include "Mage.h"
-
-Mage::Mage(const std::string& name, User* owner)
-        : PlayerCharacter(name, CharConstants::MAGE_HP, CharConstants::MAX_MAGE_DMG, CharacterType::Mage, owner) {}
-
-void Mage::ability(unsigned& dmg)
-{
-    dmg = CharConstants::MAX_MAGE_DMG + getDmgUpgrades() - dmg;
-}

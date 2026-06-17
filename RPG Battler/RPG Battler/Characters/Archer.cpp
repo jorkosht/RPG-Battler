727977f41d8c284f6dd89281c0a35e3f1b81f15c
Jorkosht <118050451+jorkosht@users.noreply.github.com>
2026-06-17 16:56:00 +0300

    Delete RPG Battler/RPG Battler/Characters/Archer.cpp

--- RPG Battler/RPG Battler/Characters/Archer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Archer.h"
-
-Archer::Archer(const std::string& name, User* owner)
-        : PlayerCharacter(name, CharConstants::ARCHER_HP, CharConstants::MAX_ARCHER_DMG, CharacterType::Archer, owner) {}
-
-void Archer::ability(unsigned& dmg)
-{
-    dmg *= CharConstants::ARCHER_DMG_MULTIPLIER;
-}
